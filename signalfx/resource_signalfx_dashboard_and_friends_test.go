e9615bc663260a404cf07a5eaf67c8a83c921adb
Cory G Watson <cwatson@signalfx.com>
2019-06-26 17:38:29 -0700

    Remove the text chart since it has it's own test

--- signalfx/resource_signalfx_dashboard_and_friends_test.go
+++ signalfx/resource_signalfx_dashboard_and_friends_test.go
@@ -47,30 +47,6 @@ resource "signalfx_heatmap_chart" "myheatmapchart0" {
     hide_timestamp = true
 }
 
-resource "signalfx_text_chart" "mynote0" {
-    name = "Important Dashboard Note"
-    description = "Lorem ipsum dolor sit amet, laudem tibique iracundia at mea. Nam posse dolores ex, nec cu adhuc putent honestatis"
-
-    markdown = <<-EOF
-    1. First ordered list item
-    2. Another item
-      * Unordered sub-list.
-    1. Actual numbers don't matter, just that it's a number
-      1. Ordered sub-list
-    4. And another item.
-
-       You can have properly indented paragraphs within list items. Notice the blank line above, and the leading spaces (at least one, but we'll use three here to also align the raw Markdown).
-
-       To have a line break without a paragraph, you will need to use two trailing spaces.⋅⋅
-       Note that this line is separate, but within the same paragraph.⋅⋅
-       (This is contrary to the typical GFM line break behaviour, where trailing spaces are not required.)
-
-    * Unordered list can use asterisks
-    - Or minuses
-    + Or pluses
-    EOF
-}
-
 resource "signalfx_dashboard_group" "mydashboardgroup0" {
     name = "My team dashboard group"
     description = "Cool dashboard group"
@@ -131,11 +107,6 @@ resource "signalfx_dashboard" "mydashboard0" {
     }
     chart {
         chart_id = "${signalfx_heatmap_chart.myheatmapchart0.id}"
-        width = 12
-        height = 1
-    }
-    chart {
-        chart_id = "${signalfx_text_chart.mynote0.id}"
         width = 12
         height = 1
     }
@@ -201,7 +172,7 @@ func TestAccCreateUpdateDashboardGroup(t *testing.T) {
 					resource.TestCheckResourceAttr("signalfx_dashboard.mydashboard0", "selected_event_overlay.0.type", "detectorEvents"),
 
 					// Charts
-					resource.TestCheckResourceAttr("signalfx_dashboard.mydashboard0", "chart.#", "3"),
+					resource.TestCheckResourceAttr("signalfx_dashboard.mydashboard0", "chart.#", "2"),
 					// We're not testing each chart because they aren't stable, TODO?
 
 					// Dashboard Group
