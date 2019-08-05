8da365494a00391bfa1d4025c89f48e8372dff31
Cory G Watson <cwatson@signalfx.com>
2019-08-05 09:45:58 -0700

    Add test

---
+++ signalfx/resource_signalfx_org_token_test.go
@@ -0,0 +1,94 @@
+package signalfx
+
+import (
+	"fmt"
+	"os"
+	"testing"
+
+	"github.com/hashicorp/terraform/helper/resource"
+	"github.com/hashicorp/terraform/terraform"
+
+	sfx "github.com/signalfx/signalfx-go"
+)
+
+const newOrgTokenConfig = `
+resource "signalfx_org_token" "myorgtokenTOK1" {
+  name = "FarToken"
+  description = "Farts"
+}
+`
+
+const updatedOrgTokenConfig = `
+resource "signalfx_org_token" "myorgtokenTOK1" {
+  name = "FarToken NEW"
+  description = "Farts"
+}
+`
+
+func TestAccCreateUpdateOrgToken(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccOrgTokenDestroy,
+		Steps: []resource.TestStep{
+			// Create It
+			{
+				Config: newOrgTokenConfig,
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckOrgTokenResourceExists,
+					resource.TestCheckResourceAttr("signalfx_org_token.myorgtokenTOK1", "name", "FarToken"),
+					resource.TestCheckResourceAttr("signalfx_org_token.myorgtokenTOK1", "description", "Farts"),
+				),
+			},
+			{
+				ResourceName:      "signalfx_org_token.myorgtokenTOK1",
+				ImportState:       true,
+				ImportStateIdFunc: testAccStateIdFunc("signalfx_org_token.myorgtokenTOK1"),
+				ImportStateVerify: true,
+			},
+			// Update Everything
+			{
+				Config: updatedOrgTokenConfig,
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckOrgTokenResourceExists,
+					resource.TestCheckResourceAttr("signalfx_org_token.myorgtokenTOK1", "name", "FarToken NEW"),
+					resource.TestCheckResourceAttr("signalfx_org_token.myorgtokenTOK1", "description", "Farts"),
+				),
+			},
+		},
+	})
+}
+
+func testAccCheckOrgTokenResourceExists(s *terraform.State) error {
+	client, _ := sfx.NewClient(os.Getenv("SFX_AUTH_TOKEN"))
+
+	for _, rs := range s.RootModule().Resources {
+		switch rs.Type {
+		case "signalfx_org_token":
+			tok, err := client.GetOrgToken(rs.Primary.ID)
+			if tok.Name != rs.Primary.ID || err != nil {
+				return fmt.Errorf("Error finding org token %s: %s", rs.Primary.ID, err)
+			}
+		default:
+			return fmt.Errorf("Unexpected resource of type: %s", rs.Type)
+		}
+	}
+	return nil
+}
+
+func testAccOrgTokenDestroy(s *terraform.State) error {
+	client, _ := sfx.NewClient(os.Getenv("SFX_AUTH_TOKEN"))
+	for _, rs := range s.RootModule().Resources {
+		switch rs.Type {
+		case "signalfx_org_token":
+			tok, _ := client.GetOrgToken(rs.Primary.ID)
+			if tok != nil {
+				return fmt.Errorf("Found deleted org token %s", rs.Primary.ID)
+			}
+		default:
+			return fmt.Errorf("Unexpected resource of type: %s", rs.Type)
+		}
+	}
+
+	return nil
+}
