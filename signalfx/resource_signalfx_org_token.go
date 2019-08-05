1be1839ee64937acb65190ee28a9b55507b56625
Cory G Watson <cwatson@signalfx.com>
2019-08-05 12:55:38 -0700

    Handle limits

--- signalfx/resource_signalfx_org_token.go
+++ signalfx/resource_signalfx_org_token.go
@@ -28,6 +28,87 @@ func orgTokenResource() *schema.Resource {
 				Default:     false,
 				Description: "Flag that controls enabling the token. If set to `true`, the token is disabled, and you can't use it for authentication. Defaults to `false`",
 			},
+
+			"host_or_usage_limits": &schema.Schema{
+				Type:          schema.TypeSet,
+				Optional:      true,
+				MaxItems:      1,
+				ConflictsWith: []string{"dpm_limits"},
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"host_notification_threshold": &schema.Schema{
+							Type:        schema.TypeInt,
+							Optional:    true,
+							Default:     -1,
+							Description: "Notification threshold for hosts",
+						},
+						"host_limit": &schema.Schema{
+							Type:        schema.TypeInt,
+							Optional:    true,
+							Default:     -1,
+							Description: "Max number of hosts that can use this token",
+						},
+						"container_notification_threshold": &schema.Schema{
+							Type:        schema.TypeInt,
+							Optional:    true,
+							Default:     -1,
+							Description: "Notification threshold for containers",
+						},
+						"container_limit": &schema.Schema{
+							Type:        schema.TypeInt,
+							Optional:    true,
+							Default:     -1,
+							Description: "Max number of containers that can use this token",
+						},
+						"custom_metrics_notification_threshold": &schema.Schema{
+							Type:        schema.TypeInt,
+							Optional:    true,
+							Default:     -1,
+							Description: "Notification threshold for custom metrics",
+						},
+						"custom_metrics_limit": &schema.Schema{
+							Type:        schema.TypeInt,
+							Optional:    true,
+							Default:     -1,
+							Description: "Max number of custom metrics that can be sent with this token",
+						},
+						"high_res_metrics_notification_threshold": &schema.Schema{
+							Type:        schema.TypeInt,
+							Optional:    true,
+							Default:     -1,
+							Description: "Notification threshold for high-res metrics",
+						},
+						"high_res_metrics_limit": &schema.Schema{
+							Type:        schema.TypeInt,
+							Optional:    true,
+							Default:     -1,
+							Description: "Max number of high-res metrics that can be sent with this token",
+						},
+					},
+				},
+			},
+
+			"dpm_limits": &schema.Schema{
+				Type:          schema.TypeSet,
+				Optional:      true,
+				MaxItems:      1,
+				ConflictsWith: []string{"host_or_usage_limits"},
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"dpm_notification_threshold": &schema.Schema{
+							Type:        schema.TypeInt,
+							Optional:    true,
+							Default:     -1,
+							Description: "DPM level at which SignalFx sends the notification for this token. If you don't specify a notification, SignalFx sends the generic notification.",
+						},
+						"dpm_limit": &schema.Schema{
+							Type:        schema.TypeInt,
+							Required:    true,
+							Description: "The datapoints per minute (dpm) limit for this token. If you exceed this limit, SignalFx sends out an alert.",
+						},
+					},
+				},
+			},
 		},
 
 		Create: orgTokenCreate,
@@ -42,11 +123,74 @@ func orgTokenResource() *schema.Resource {
 }
 
 func getPayloadOrgToken(d *schema.ResourceData) *orgtoken.CreateUpdateTokenRequest {
-	return &orgtoken.CreateUpdateTokenRequest{
+	token := &orgtoken.CreateUpdateTokenRequest{
 		Name:        d.Get("name").(string),
 		Description: d.Get("description").(string),
 		Disabled:    d.Get("disabled").(bool),
 	}
+
+	if hostLimits, ok := d.GetOk("host_or_usage_limits"); ok {
+		hostLimits := hostLimits.(*schema.Set).List()[0].(map[string]interface{})
+		categoryQuotas := &orgtoken.UsageLimits{}
+		categoryThresholds := &orgtoken.UsageLimits{}
+		if threshold, ok := hostLimits["host_notification_threshold"]; ok && threshold.(int) != -1 {
+			v := int64(threshold.(int))
+			categoryThresholds.HostThreshold = &v
+		}
+		if threshold, ok := hostLimits["container_notification_threshold"]; ok && threshold.(int) != -1 {
+			v := int64(threshold.(int))
+			categoryThresholds.ContainerThreshold = &v
+		}
+		if threshold, ok := hostLimits["custom_metrics_notification_threshold"]; ok && threshold.(int) != -1 {
+			v := int64(threshold.(int))
+			categoryThresholds.CustomMetricThreshold = &v
+		}
+		if threshold, ok := hostLimits["high_res_metrics_notification_threshold"]; ok && threshold.(int) != -1 {
+			v := int64(threshold.(int))
+			categoryThresholds.HighResMetricThreshold = &v
+		}
+
+		if limit, ok := hostLimits["host_limit"]; ok && limit.(int) != -1 {
+			v := int64(limit.(int))
+			categoryQuotas.HostThreshold = &v
+		}
+		if limit, ok := hostLimits["container_limit"]; ok && limit.(int) != -1 {
+			v := int64(limit.(int))
+			categoryQuotas.ContainerThreshold = &v
+		}
+		if limit, ok := hostLimits["custom_metrics_limit"]; ok && limit.(int) != -1 {
+			v := int64(limit.(int))
+			categoryQuotas.CustomMetricThreshold = &v
+		}
+		if limit, ok := hostLimits["high_res_metrics_limit"]; ok && limit.(int) != -1 {
+			v := int64(limit.(int))
+			categoryQuotas.HighResMetricThreshold = &v
+		}
+
+		limits := &orgtoken.Limit{
+			CategoryQuota:                 categoryQuotas,
+			CategoryNotificationThreshold: categoryThresholds,
+		}
+
+		token.Limits = limits
+	}
+
+	if dpmLimits, ok := d.GetOk("dpm_limits"); ok {
+		dpmLimits := dpmLimits.(*schema.Set).List()[0].(map[string]interface{})
+		dq := int32(dpmLimits["dpm_limit"].(int))
+		limits := &orgtoken.Limit{
+			DpmQuota: &dq,
+		}
+
+		if limit, ok := dpmLimits["dpm_notification_threshold"]; ok {
+			v := int32(limit.(int))
+			limits.DpmNotificationThreshold = &v
+		}
+
+		token.Limits = limits
+	}
+
+	return token
 }
 
 func orgTokenCreate(d *schema.ResourceData, meta interface{}) error {
@@ -76,6 +220,56 @@ func orgTokenAPIToTF(d *schema.ResourceData, t *orgtoken.Token) error {
 	}
 	if err := d.Set("disabled", t.Disabled); err != nil {
 		return err
+	}
+
+	if t.Limits != nil {
+		limits := t.Limits
+		if limits.DpmQuota != nil {
+			dpmStuff := map[string]interface{}{
+				"dpm_limit": limits.DpmQuota,
+			}
+			if limits.DpmNotificationThreshold != nil {
+				dpmStuff["dpm_notification_threshold"] = limits.DpmNotificationThreshold
+			}
+			if err := d.Set("dpm_limit", dpmStuff); err != nil {
+				return err
+			}
+		} else {
+			usageStuff := map[string]interface{}{}
+			if limits.CategoryQuota != nil && *limits.CategoryQuota != (orgtoken.UsageLimits{}) {
+				cq := limits.CategoryQuota
+				if cq.HostThreshold != nil {
+					usageStuff["host_limit"] = *cq.HostThreshold
+				}
+				if cq.ContainerThreshold != nil {
+					usageStuff["container_limit"] = *cq.ContainerThreshold
+				}
+				if cq.CustomMetricThreshold != nil {
+					usageStuff["custom_metrics_limit"] = *cq.CustomMetricThreshold
+				}
+				if cq.HighResMetricThreshold != nil {
+					usageStuff["high_res_metrics_limit"] = *cq.HighResMetricThreshold
+				}
+			}
+			if limits.CategoryNotificationThreshold != nil && *limits.CategoryNotificationThreshold != (orgtoken.UsageLimits{}) {
+				cnt := limits.CategoryNotificationThreshold
+				if cnt.HostThreshold != nil {
+					usageStuff["host_notification_threshold"] = *cnt.HostThreshold
+				}
+				if cnt.ContainerThreshold != nil {
+					usageStuff["container_notification_threshold"] = *cnt.ContainerThreshold
+				}
+				if cnt.CustomMetricThreshold != nil {
+					usageStuff["custom_metrics_notification_threshold"] = *cnt.CustomMetricThreshold
+				}
+				if cnt.HighResMetricThreshold != nil {
+					usageStuff["high_res_metrics_notification_threshold"] = *cnt.HighResMetricThreshold
+				}
+			}
+			if err := d.Set("host_or_usage_limits", []interface{}{usageStuff}); err != nil {
+				return err
+			}
+		}
 	}
 
 	return nil
--- signalfx/resource_signalfx_org_token_test.go
+++ signalfx/resource_signalfx_org_token_test.go
@@ -15,6 +15,17 @@ const newOrgTokenConfig = `
 resource "signalfx_org_token" "myorgtokenTOK1" {
   name = "FarToken"
   description = "Farts"
+
+  host_or_usage_limits {
+    host_limit = 100
+    host_notification_threshold = 90
+    container_limit = 200
+    container_notification_threshold = 180
+    custom_metrics_limit = 1000
+    custom_metrics_notification_threshold = 900
+    high_res_metrics_limit = 1000
+    high_res_metrics_notification_threshold = 900
+  }
 }
 `
 
@@ -22,6 +33,17 @@ const updatedOrgTokenConfig = `
 resource "signalfx_org_token" "myorgtokenTOK1" {
   name = "FarToken NEW"
   description = "Farts"
+
+  host_or_usage_limits {
+    host_limit = 100
+    host_notification_threshold = 90
+    container_limit = 200
+    container_notification_threshold = 180
+    custom_metrics_limit = 1000
+    custom_metrics_notification_threshold = 900
+    high_res_metrics_limit = 1000
+    high_res_metrics_notification_threshold = 900
+  }
 }
 `
 
