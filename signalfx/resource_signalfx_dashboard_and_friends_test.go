d17a22fdfad10c232aedb1cb9f5f767a3cdbd9c8
Cory G Watson <cwatson@signalfx.com>
2019-12-16 10:58:28 -0800

    Working with tests!

--- signalfx/resource_signalfx_dashboard_and_friends_test.go
+++ signalfx/resource_signalfx_dashboard_and_friends_test.go
@@ -155,6 +155,18 @@ resource "signalfx_dashboard" "mydashboard0" {
         height = 1
     }
 }
+
+resource "signalfx_data_link" "my_data_link" {
+    property_name = "pname"
+    property_value = "pvalue"
+
+    target_signalfx_dashboard {
+      is_default = true
+      name = "sfx_dash"
+			dashboard_group_id = "${signalfx_dashboard_group.mydashboardgroup0.id}"
+			dashboard_id = "${signalfx_dashboard.mydashboard0.id}"
+    }
+}
 `
 
 const updatedDashConfig = `
@@ -301,6 +313,18 @@ resource "signalfx_dashboard" "mydashboard0" {
         height = 1
     }
 }
+
+resource "signalfx_data_link" "my_data_link" {
+    property_name = "pname"
+    property_value = "pvalue2"
+
+    target_signalfx_dashboard {
+      is_default = true
+      name = "sfx_dash"
+			dashboard_group_id = "${signalfx_dashboard_group.mydashboardgroup0.id}"
+			dashboard_id = "${signalfx_dashboard.mydashboard0.id}"
+    }
+}
 `
 
 func TestAccCreateUpdateDashboardGroup(t *testing.T) {
@@ -418,6 +442,11 @@ func testAccCheckDashboardGroupResourceExists(s *terraform.State) error {
 			if dashgroup.Id != rs.Primary.ID || err != nil {
 				return fmt.Errorf("Error finding dashboard group %s: %s", rs.Primary.ID, err)
 			}
+		case "signalfx_data_link":
+			dl, err := client.GetDataLink(rs.Primary.ID)
+			if err != nil || dl.Id != rs.Primary.ID {
+				return fmt.Errorf("Error finding data link %s: %s", rs.Primary.ID, err)
+			}
 		default:
 			return fmt.Errorf("Unexpected resource of type: %s", rs.Type)
 		}
@@ -443,6 +472,11 @@ func testAccDashboardGroupDestroy(s *terraform.State) error {
 			dashgroup, _ := client.GetDashboardGroup(rs.Primary.ID)
 			if dashgroup != nil {
 				return fmt.Errorf("Found deleted dashboard group %s", rs.Primary.ID)
+			}
+		case "signalfx_data_link":
+			dl, _ := client.GetDataLink(rs.Primary.ID)
+			if dl != nil {
+				return fmt.Errorf("Found deleted data link %s", rs.Primary.ID)
 			}
 		default:
 			return fmt.Errorf("Unexpected resource of type: %s", rs.Type)
---
+++ signalfx/resource_signalfx_data_link.go
@@ -0,0 +1,374 @@
+package signalfx
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"strings"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
+	"github.com/signalfx/signalfx-go/datalink"
+	"github.com/signalfx/signalfx-go/util"
+)
+
+func dataLinkResource() *schema.Resource {
+	return &schema.Resource{
+		Schema: map[string]*schema.Schema{
+			"property_name": &schema.Schema{
+				Type:        schema.TypeString,
+				Optional:    true,
+				Description: "Name (key) of the metadata that's the trigger of a data link. If you specify `property_value`, you must specify `property_name`.",
+			},
+			"property_value": &schema.Schema{
+				Type:        schema.TypeString,
+				Optional:    true,
+				Description: "Value of the metadata that's the trigger of a data link. If you specify this property, you must also specify `property_name`.",
+			},
+			"target_signalfx_dashboard": &schema.Schema{
+				Type:        schema.TypeSet,
+				Optional:    true,
+				Description: "Link to a SignalFx dashboard",
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"dashboard_group_id": &schema.Schema{
+							Type:        schema.TypeString,
+							Required:    true,
+							Description: "SignalFx-assigned ID of the dashboard link target's dashboard group",
+						},
+						"dashboard_id": &schema.Schema{
+							Type:        schema.TypeString,
+							Required:    true,
+							Description: "SignalFx-assigned ID of the dashboard link target",
+						},
+						"is_default": &schema.Schema{
+							Type:        schema.TypeBool,
+							Default:     true,
+							Optional:    true,
+							Description: "Flag that designates a target as the default for a data link object.",
+						},
+						"name": &schema.Schema{
+							Type:        schema.TypeString,
+							Required:    true,
+							Description: "User-assigned target name. Use this value to differentiate between the link targets for a data link object.",
+						},
+					},
+				},
+			},
+			"target_external_url": &schema.Schema{
+				Type:        schema.TypeSet,
+				Optional:    true,
+				Description: "Link to an external URL",
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"is_default": &schema.Schema{
+							Type:        schema.TypeBool,
+							Default:     true,
+							Optional:    true,
+							Description: "Flag that designates a target as the default for a data link object.",
+						},
+						"name": &schema.Schema{
+							Type:        schema.TypeString,
+							Required:    true,
+							Description: "User-assigned target name. Use this value to differentiate between the link targets for a data link object.",
+						},
+						"time_format": &schema.Schema{
+							Type:        schema.TypeString,
+							Default:     datalink.ISO8601,
+							Optional:    true,
+							Description: "Designates the format of minimumTimeWindow in the same data link target object.",
+							ValidateFunc: validation.StringInSlice([]string{
+								string(datalink.ISO8601), string(datalink.Epoch),
+							}, false),
+						},
+						"url": &schema.Schema{
+							Type:        schema.TypeString,
+							Required:    true,
+							Description: "URL string for a Splunk instance or external system data link target.",
+						},
+						"property_key_mapping": &schema.Schema{
+							Type:        schema.TypeMap,
+							Optional:    true,
+							Description: "Describes the relationship between SignalFx metadata keys and external system properties when the key names are different",
+							Elem: &schema.Schema{
+								Type: schema.TypeString,
+							},
+						},
+						"minimum_time_window": &schema.Schema{
+							Type:        schema.TypeString,
+							Optional:    true,
+							Default:     "6000",
+							Description: "The minimum time window for a search sent to an external site. Depends on the value set for `time_format`.",
+						},
+					},
+				},
+			},
+			"target_splunk": &schema.Schema{
+				Type:        schema.TypeSet,
+				Optional:    true,
+				Description: "Link to a Splunk instance",
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"is_default": &schema.Schema{
+							Type:        schema.TypeBool,
+							Default:     true,
+							Optional:    true,
+							Description: "Flag that designates a target as the default for a data link object.",
+						},
+						"name": &schema.Schema{
+							Type:        schema.TypeString,
+							Required:    true,
+							Description: "User-assigned target name. Use this value to differentiate between the link targets for a data link object.",
+						},
+						"property_key_mapping": &schema.Schema{
+							Type:        schema.TypeMap,
+							Optional:    true,
+							Description: "Describes the relationship between SignalFx metadata keys and external system properties when the key names are different",
+							Elem: &schema.Schema{
+								Type: schema.TypeString,
+							},
+						},
+					},
+				},
+			},
+		},
+
+		Create: dataLinkCreate,
+		Read:   dataLinkRead,
+		Update: dataLinkUpdate,
+		Delete: dataLinkDelete,
+		Exists: dataLinkExists,
+		Importer: &schema.ResourceImporter{
+			State: schema.ImportStatePassthrough,
+		},
+	}
+}
+
+func getPayloadDataLink(d *schema.ResourceData) (*datalink.CreateUpdateDataLinkRequest, error) {
+	dataLink := &datalink.CreateUpdateDataLinkRequest{}
+
+	if val, ok := d.GetOk("property_value"); ok {
+		if name, ok := d.GetOk("property_name"); ok {
+			dataLink.PropertyValue = val.(string)
+			dataLink.PropertyName = name.(string)
+		} else {
+			return dataLink, fmt.Errorf("Must supply a property_name when supplying a property_value")
+		}
+	}
+
+	if val, ok := d.GetOk("target_signalfx_dashboard"); ok {
+		sfxDashes := val.(*schema.Set).List()
+
+		for _, tfLink := range sfxDashes {
+			tfLink := tfLink.(map[string]interface{})
+			dl := &datalink.Target{
+				DashboardGroupId: tfLink["dashboard_group_id"].(string),
+				DashboardId:      tfLink["dashboard_id"].(string),
+				Name:             tfLink["name"].(string),
+				IsDefault:        tfLink["is_default"].(bool),
+				Type:             datalink.INTERNAL_LINK,
+			}
+			if val, ok := tfLink["dashboard_group_name"]; ok {
+				dl.DashboardGroupName = val.(string)
+			}
+			if val, ok := tfLink["dashboard_name"]; ok {
+				dl.DashboardName = val.(string)
+			}
+			dataLink.Targets = append(dataLink.Targets, dl)
+		}
+	}
+
+	if val, ok := d.GetOk("target_splunk"); ok {
+		splkDashes := val.(*schema.Set).List()
+
+		for _, tfLink := range splkDashes {
+			tfLink := tfLink.(map[string]interface{})
+			dl := &datalink.Target{
+				Name:      tfLink["name"].(string),
+				IsDefault: tfLink["is_default"].(bool),
+				Type:      datalink.SPLUNK_LINK,
+			}
+
+			if v, ok := tfLink["property_key_mapping"]; ok {
+				pkMap := map[string]string{}
+				for key, value := range v.(map[string]interface{}) {
+					pkMap[key] = value.(string)
+				}
+				dl.PropertyKeyMapping = pkMap
+			}
+			dataLink.Targets = append(dataLink.Targets, dl)
+		}
+	}
+
+	if val, ok := d.GetOk("target_external_url"); ok {
+		exURLs := val.(*schema.Set).List()
+
+		for _, tfLink := range exURLs {
+			tfLink := tfLink.(map[string]interface{})
+
+			dl := &datalink.Target{
+				Name:              tfLink["name"].(string),
+				IsDefault:         tfLink["is_default"].(bool),
+				MinimumTimeWindow: util.StringOrInteger(tfLink["minimum_time_window"].(string)),
+				URL:               tfLink["url"].(string),
+				Type:              datalink.EXTERNAL_LINK,
+			}
+
+			if tfLink["time_format"].(string) == "Epoch" {
+				dl.TimeFormat = datalink.Epoch
+			} else {
+				dl.TimeFormat = datalink.ISO8601
+			}
+
+			if v, ok := tfLink["property_key_mapping"]; ok {
+				fmt.Printf("[DEBUG] SignalFx: GOT A KEY MAPPING")
+				pkMap := map[string]string{}
+				for key, value := range v.(map[string]interface{}) {
+					pkMap[key] = value.(string)
+				}
+				dl.PropertyKeyMapping = pkMap
+			} else {
+				fmt.Printf("[DEBUG] SignalFx: NONONO A KEY MAPPING")
+			}
+
+			dataLink.Targets = append(dataLink.Targets, dl)
+		}
+	}
+
+	if len(dataLink.Targets) < 1 {
+		return dataLink, fmt.Errorf("You must provide one or more of `target_signalfx_dashboard`, `target_external_url`, or `target_splunk`")
+	}
+
+	return dataLink, nil
+}
+
+func dataLinkCreate(d *schema.ResourceData, meta interface{}) error {
+	config := meta.(*signalfxConfig)
+	payload, err := getPayloadDataLink(d)
+	if err != nil {
+		return err
+	}
+
+	debugOutput, _ := json.Marshal(payload)
+	log.Printf("[DEBUG] SignalFx: Create Data Link Payload: %s", string(debugOutput))
+
+	dl, err := config.Client.CreateDataLink(payload)
+	if err != nil {
+		return err
+	}
+	d.SetId(dl.Id)
+	return dataLinkAPIToTF(d, dl)
+}
+
+func dataLinkAPIToTF(d *schema.ResourceData, dl *datalink.DataLink) error {
+	debugOutput, _ := json.Marshal(dl)
+	log.Printf("[DEBUG] SignalFx: Got Data Link to enState: %s", string(debugOutput))
+
+	if err := d.Set("property_value", dl.PropertyValue); err != nil {
+		return err
+	}
+	if err := d.Set("property_name", dl.PropertyName); err != nil {
+		return err
+	}
+
+	var internalLinks []map[string]interface{}
+	var externalLinks []map[string]interface{}
+	var splunkLinks []map[string]interface{}
+
+	for _, t := range dl.Targets {
+		switch t.Type {
+		case datalink.INTERNAL_LINK:
+			tfTarget := map[string]interface{}{
+				"name":               t.Name,
+				"dashboard_group_id": t.DashboardGroupId,
+				"dashboard_id":       t.DashboardId,
+				"is_default":         t.IsDefault,
+			}
+			internalLinks = append(internalLinks, tfTarget)
+		case datalink.EXTERNAL_LINK:
+			tfTarget := map[string]interface{}{
+				"name":                 t.Name,
+				"is_default":           t.IsDefault,
+				"minimum_time_window":  t.MinimumTimeWindow,
+				"time_format":          t.TimeFormat,
+				"url":                  t.URL,
+				"property_key_mapping": t.PropertyKeyMapping,
+			}
+			externalLinks = append(externalLinks, tfTarget)
+		case datalink.SPLUNK_LINK:
+			tfTarget := map[string]interface{}{
+				"name":                 t.Name,
+				"is_default":           t.IsDefault,
+				"property_key_mapping": t.PropertyKeyMapping,
+			}
+			splunkLinks = append(splunkLinks, tfTarget)
+		default:
+			return fmt.Errorf("Unknown link type: %s", t.Type)
+		}
+	}
+	if internalLinks != nil && len(internalLinks) > 0 {
+		if err := d.Set("target_signalfx_dashboard", internalLinks); err != nil {
+			return err
+		}
+	}
+	if externalLinks != nil && len(externalLinks) > 0 {
+		if err := d.Set("target_external_url", externalLinks); err != nil {
+			return err
+		}
+	}
+	if splunkLinks != nil && len(splunkLinks) > 0 {
+		if err := d.Set("target_splunk", splunkLinks); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func dataLinkRead(d *schema.ResourceData, meta interface{}) error {
+	config := meta.(*signalfxConfig)
+	fmt.Printf("[DEBUG] SignalFx: Looking for data link %s", d.Id())
+	dl, err := config.Client.GetDataLink(d.Id())
+	if err != nil {
+		return err
+	}
+
+	return dataLinkAPIToTF(d, dl)
+}
+
+func dataLinkUpdate(d *schema.ResourceData, meta interface{}) error {
+	config := meta.(*signalfxConfig)
+	payload, err := getPayloadDataLink(d)
+	if err != nil {
+		return err
+	}
+	debugOutput, _ := json.Marshal(payload)
+	log.Printf("[DEBUG] SignalFx: Update Data Link Payload: %s", string(debugOutput))
+
+	dl, err := config.Client.UpdateDataLink(d.Id(), payload)
+	if err != nil {
+		return err
+	}
+	log.Printf("[DEBUG] SignalFx: Update Data Link Response: %v", dl)
+
+	d.SetId(dl.Id)
+	return dataLinkAPIToTF(d, dl)
+}
+
+func dataLinkDelete(d *schema.ResourceData, meta interface{}) error {
+	config := meta.(*signalfxConfig)
+
+	return config.Client.DeleteDataLink(d.Id())
+}
+
+func dataLinkExists(d *schema.ResourceData, meta interface{}) (bool, error) {
+	config := meta.(*signalfxConfig)
+	_, err := config.Client.GetDataLink(d.Id())
+	if err != nil {
+		if strings.Contains(err.Error(), "404") {
+			return false, nil
+		}
+		return false, err
+	}
+	return true, nil
+}
