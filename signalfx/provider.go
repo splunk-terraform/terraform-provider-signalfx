1c2414bca3e6cf0b1025ce4d87d2a6551ea590ad
Cory G Watson <cwatson@signalfx.com>
2020-08-11 10:47:00 -0700

    Put back old UA now that API is ok with it.

--- signalfx/provider.go
+++ signalfx/provider.go
@@ -158,10 +158,7 @@ func signalfxConfigure(data *schema.ResourceData) (interface{}, error) {
 	})
 
 	pv := version.ProviderVersion
-	// This U-A prefix is hardcoded (rather than using some programmatic accessor)
-	// because doing so fixes #208, a slow down in HTTP POSTs for dashboards.
-	// Might be safe to remove later.
-	providerUserAgent := fmt.Sprintf("Go-http-client/1.1 Terraform/%s terraform-provider-signalfx/%s", sfxProvider.TerraformVersion, pv)
+	providerUserAgent := fmt.Sprintf("Terraform/%s terraform-provider-signalfx/%s", sfxProvider.TerraformVersion, pv)
 
 	totalTimeoutSeconds := data.Get("timeout_seconds").(int)
 	log.Printf("[DEBUG] SignalFx: HTTP Timeout is %d seconds", totalTimeoutSeconds)
