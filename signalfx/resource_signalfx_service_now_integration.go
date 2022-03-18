0452ff27d9a0c4a47f1d8458e6b3a8015f13b10f
Luke Sterkowicz <lsterkowicz@splunk.com>
2022-03-18 18:02:54 +0100

    Add ServiceNow integration

---
+++ signalfx/resource_signalfx_service_now_integration.go
@@ -0,0 +1,180 @@
+/*
+ * Resource for ServiceNow integration
+ *
+ * https://dev.splunk.com/observability/reference/api/integrations/latest
+ * https://docs.splunk.com/Observability/admin/notif-services/servicenow.html
+ */
+
+package signalfx
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
+	"github.com/signalfx/signalfx-go/integration"
+)
+
+const (
+	serviceNowIntegrationName = "Service Now"
+	serviceNowTypeIncident    = "Incident"
+	serviceNowTypeProblem     = "Problem"
+)
+
+func integrationServiceNowResource() *schema.Resource {
+	return &schema.Resource{
+		Schema: map[string]*schema.Schema{
+			"name": {
+				Type:        schema.TypeString,
+				Required:    true,
+				Description: "Name of the integration",
+			},
+			"enabled": {
+				Type:        schema.TypeBool,
+				Required:    true,
+				Description: "Whether the integration is enabled or not",
+			},
+			"username": {
+				Type:        schema.TypeString,
+				Required:    true,
+				Description: "User name used to authenticate the Service Now integration.",
+			},
+			"password": {
+				Type:        schema.TypeString,
+				Required:    true,
+				Sensitive:   true,
+				Description: "Password used to authenticate the Service Now integration.",
+			},
+			"instance_name": {
+				Type:        schema.TypeString,
+				Required:    true,
+				Description: "Name of the ServiceNow instance, for example `myInstances.service-now.com`.",
+			},
+			"issue_type": {
+				Type:         schema.TypeString,
+				Required:     true,
+				ValidateFunc: validation.StringInSlice([]string{serviceNowTypeIncident, serviceNowTypeProblem}, false),
+				Description:  fmt.Sprintf("The type of issue in standard ITIL terminology. The allowed values are '%s' and '%s'.", serviceNowTypeIncident, serviceNowTypeProblem),
+			},
+			"alert_triggered_payload_template": {
+				Type:        schema.TypeString,
+				Optional:    true,
+				Description: "An optional template that Observability Cloud uses to create the ServiceNow POST JSON payloads when an alert sends a notification to ServiceNow. Use this optional field to send the values of Observability Cloud alert properties to specific fields in ServiceNow. See API reference for details.",
+			},
+			"alert_resolved_payload_template": {
+				Type:        schema.TypeString,
+				Optional:    true,
+				Description: "An optional template that Observability Cloud uses to create the ServiceNow PUT JSON payloads when an alert is cleared in ServiceNow. Use this optional field to send the values of Observability Cloud alert properties to specific fields in ServiceNow. See API reference for details.",
+			},
+		},
+
+		Create: integrationServiceNowCreate,
+		Read:   integrationServiceNowRead,
+		Update: integrationServiceNowUpdate,
+		Delete: integrationServiceNowDelete,
+		Exists: integrationServiceNowExists,
+		Importer: &schema.ResourceImporter{
+			State: schema.ImportStatePassthrough,
+		},
+	}
+}
+
+func getServiceNowIntegration(d *schema.ResourceData) *integration.ServiceNowIntegration {
+	snow := &integration.ServiceNowIntegration{
+		Type:         integration.SERVICE_NOW,
+		Name:         d.Get("name").(string),
+		Enabled:      d.Get("enabled").(bool),
+		InstanceName: d.Get("instance_name").(string),
+		IssueType:    d.Get("issue_type").(string),
+		Username:     d.Get("username").(string),
+		Password:     d.Get("password").(string),
+	}
+	if val, ok := d.GetOk("alert_triggered_payload_template"); ok {
+		snow.AlertTriggeredPayloadTemplate = val.(string)
+	}
+	if val, ok := d.GetOk("alert_resolved_payload_template"); ok {
+		snow.AlertResolvedPayloadTemplate = val.(string)
+	}
+	return snow
+}
+
+func copyServiceNowIntegrationToResource(d *schema.ResourceData, snow *integration.ServiceNowIntegration) error {
+	// API doesn't return username and password, so we ignore them.
+	if err := d.Set("name", snow.Name); err != nil {
+		return err
+	}
+	if err := d.Set("enabled", snow.Enabled); err != nil {
+		return err
+	}
+	if err := d.Set("instance_name", snow.InstanceName); err != nil {
+		return err
+	}
+	if err := d.Set("issue_type", snow.IssueType); err != nil {
+		return err
+	}
+	if snow.AlertTriggeredPayloadTemplate != "" {
+		if err := d.Set("alert_triggered_payload_template", snow.AlertTriggeredPayloadTemplate); err != nil {
+			return err
+		}
+	}
+	if snow.AlertResolvedPayloadTemplate != "" {
+		if err := d.Set("alert_resolved_payload_template", snow.AlertResolvedPayloadTemplate); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func integrationServiceNowExists(d *schema.ResourceData, meta interface{}) (bool, error) {
+	config := meta.(*signalfxConfig)
+
+	_, err := config.Client.GetServiceNowIntegration(context.TODO(), d.Id())
+	return handleIntegrationExists(err)
+}
+
+func integrationServiceNowRead(d *schema.ResourceData, meta interface{}) error {
+	config := meta.(*signalfxConfig)
+
+	in, err := config.Client.GetServiceNowIntegration(context.TODO(), d.Id())
+	if !handleIntegrationRead(err, d) {
+		return err
+	}
+	logIntegrationResponse(in, serviceNowIntegrationName)
+
+	return copyServiceNowIntegrationToResource(d, in)
+}
+
+func integrationServiceNowCreate(d *schema.ResourceData, meta interface{}) error {
+	config := meta.(*signalfxConfig)
+	out := getServiceNowIntegration(d)
+	logIntegrationCreateRequest(out, serviceNowIntegrationName)
+
+	in, err := config.Client.CreateServiceNowIntegration(context.TODO(), out)
+	if !handleIntegrationChange(err, d, in) {
+		return err
+	}
+	logIntegrationResponse(in, serviceNowIntegrationName)
+
+	return copyServiceNowIntegrationToResource(d, in)
+}
+
+func integrationServiceNowUpdate(d *schema.ResourceData, meta interface{}) error {
+	config := meta.(*signalfxConfig)
+	out := getServiceNowIntegration(d)
+	logIntegrationUpdateRequest(out, serviceNowIntegrationName)
+
+	in, err := config.Client.UpdateServiceNowIntegration(context.TODO(), d.Id(), out)
+	if !handleIntegrationChange(err, d, in) {
+		return err
+	}
+	logIntegrationResponse(in, serviceNowIntegrationName)
+
+	return copyServiceNowIntegrationToResource(d, in)
+}
+
+func integrationServiceNowDelete(d *schema.ResourceData, meta interface{}) error {
+	config := meta.(*signalfxConfig)
+
+	return config.Client.DeleteServiceNowIntegration(context.TODO(), d.Id())
+}
---
+++ signalfx/resource_signalfx_service_now_integration_test.go
@@ -0,0 +1,76 @@
+package signalfx
+
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+)
+
+const newIntegrationServiceNowConfig = `
+resource "signalfx_service_now_integration" "snow_myresXX" {
+    name = "SNOW #1"
+    enabled = false
+    username = "thisis_me"
+    password = "youd0ntsee1t"
+    instance_name = "myinst.service-now.com"
+	issue_type = "Incident"
+}
+`
+
+const updatedIntegrationServiceNowConfig = `
+resource "signalfx_service_now_integration" "snow_myresXX" {
+    name = "SNOW #222"
+    enabled = false
+    username = "thisis_me"
+    password = "youd0ntsee1t"
+    instance_name = "myinst.service-now.com"
+	issue_type = "Problem"
+	alert_triggered_payload_template = "doesn't really matter"
+	alert_resolved_payload_template = "nope"
+}`
+
+func TestAccCreateUpdateIntegrationServiceNow(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCreateCheckDestroyIntegrationResource("signalfx_service_now_integration"),
+		Steps: []resource.TestStep{
+			// Create It
+			{
+				Config: newIntegrationServiceNowConfig,
+				Check: resource.ComposeTestCheckFunc(
+					testAccCreateCheckIntegrationResource("signalfx_service_now_integration"),
+					resource.TestCheckResourceAttr("signalfx_service_now_integration.snow_myresXX", "name", "SNOW #1"),
+					resource.TestCheckResourceAttr("signalfx_service_now_integration.snow_myresXX", "enabled", "false"),
+					resource.TestCheckResourceAttr("signalfx_service_now_integration.snow_myresXX", "username", "thisis_me"),
+					resource.TestCheckResourceAttr("signalfx_service_now_integration.snow_myresXX", "password", "youd0ntsee1t"),
+					resource.TestCheckResourceAttr("signalfx_service_now_integration.snow_myresXX", "instance_name", "myinst.service-now.com"),
+					resource.TestCheckResourceAttr("signalfx_service_now_integration.snow_myresXX", "issue_type", "Incident"),
+				),
+			},
+			{
+				ResourceName:      "signalfx_service_now_integration.snow_myresXX",
+				ImportState:       true,
+				ImportStateIdFunc: testAccStateIdFunc("signalfx_service_now_integration.snow_myresXX"),
+				ImportStateVerify: true,
+				// The API doesn't return this value, so blow it up
+				ImportStateVerifyIgnore: []string{"username", "password"},
+			},
+			// Update It
+			{
+				Config: updatedIntegrationServiceNowConfig,
+				Check: resource.ComposeTestCheckFunc(
+					testAccCreateCheckIntegrationResource("signalfx_service_now_integration"),
+					resource.TestCheckResourceAttr("signalfx_service_now_integration.snow_myresXX", "name", "SNOW #222"),
+					resource.TestCheckResourceAttr("signalfx_service_now_integration.snow_myresXX", "enabled", "false"),
+					resource.TestCheckResourceAttr("signalfx_service_now_integration.snow_myresXX", "username", "thisis_me"),
+					resource.TestCheckResourceAttr("signalfx_service_now_integration.snow_myresXX", "password", "youd0ntsee1t"),
+					resource.TestCheckResourceAttr("signalfx_service_now_integration.snow_myresXX", "instance_name", "myinst.service-now.com"),
+					resource.TestCheckResourceAttr("signalfx_service_now_integration.snow_myresXX", "issue_type", "Problem"),
+					resource.TestCheckResourceAttr("signalfx_service_now_integration.snow_myresXX", "alert_triggered_payload_template", "doesn't really matter"),
+					resource.TestCheckResourceAttr("signalfx_service_now_integration.snow_myresXX", "alert_resolved_payload_template", "nope"),
+				),
+			},
+		},
+	})
+}
