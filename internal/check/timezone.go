59f237e350e1d2a3e2d3664e64621f45b704e6a8
Sean Marciniak <30928402+MovieStoreGuy@users.noreply.github.com>
2024-10-05 02:58:52 +0930

    Feature: adding new package `check` (#505)
    * Add support for check functions
    
    * Remove unneeded check for error

---
+++ internal/check/timezone.go
@@ -0,0 +1,28 @@
+package check
+
+import (
+	"fmt"
+	"time"
+	_ "time/tzdata" // Importing time zone database to ensure there is failover option
+
+	"github.com/hashicorp/go-cty/cty"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+
+	tfext "github.com/splunk-terraform/terraform-provider-signalfx/internal/tfextension"
+)
+
+func TimeZoneLocation() schema.SchemaValidateDiagFunc {
+	return func(i interface{}, p cty.Path) diag.Diagnostics {
+		tz, ok := i.(string)
+		if !ok {
+			return tfext.AsErrorDiagnostics(
+				fmt.Errorf("expected %v as string", i),
+				p,
+			)
+		}
+		_, err := time.LoadLocation(tz)
+		return tfext.AsErrorDiagnostics(err, p)
+
+	}
+}
