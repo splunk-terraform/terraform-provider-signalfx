c9901dfde6aa36e478b99bbdf011b248ddfc6ab6
Sean Marciniak <smarciniak@splunk.com>
2024-10-04 14:58:27 +0930

    Adding tfextension package

---
+++ internal/tfextension/diag.go
@@ -0,0 +1,35 @@
+package tfext
+
+import (
+	"slices"
+
+	"github.com/hashicorp/go-cty/cty"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+)
+
+// AppendDiagnostics is to be used similar to combining errors together
+// so they are reported at the same time to provide all details at once.
+func AppendDiagnostics(diags diag.Diagnostics, values ...diag.Diagnostic) diag.Diagnostics {
+	return slices.Concat(diags, values)
+}
+
+// AsErrorDiagnostics is the same as `diag.FromErr`, however, it allow allows
+// adding the attribute values that are provided in CRUD operations.
+func AsErrorDiagnostics(err error, path ...cty.Path) diag.Diagnostics {
+	return newDiagnostics(diag.Error, err, path...)
+}
+
+// AsWarnDiagnostics is the same as `diag.FromErr`, however, it sets the severity as Warning
+// and allows for appending the attribute path as part of the values.
+func AsWarnDiagnostics(err error, path ...cty.Path) diag.Diagnostics {
+	return newDiagnostics(diag.Warning, err, path...)
+}
+
+func newDiagnostics(sev diag.Severity, summary error, path ...cty.Path) diag.Diagnostics {
+	if summary == nil {
+		return nil
+	}
+	return diag.Diagnostics{
+		{Severity: sev, Summary: summary.Error(), AttributePath: slices.Concat(path...)},
+	}
+}
---
+++ internal/tfextension/encoding.go
@@ -0,0 +1,19 @@
+package tfext
+
+import "github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+
+// Using generics for the function signature instead of the typical any parameter
+// to help reduce repeated lines of code needing to cast to or from an empty interface.
+type (
+	// DecodeTerraformFunc is used to define the method signature used to convert
+	// the terraform state data into the expected API type to be used.
+	//
+	// This is to be used when interfacing with other packages.
+	DecodeTerraformFunc[T any] func(rd *schema.ResourceData) (*T, error)
+
+	// EncodeTerraformFunc is used to write the API response data into
+	// the terraform state.
+	//
+	// This is to be used when interfacing with other packages.
+	EncodeTerraformFunc[T any] func(t *T, rd *schema.ResourceData) error
+)
