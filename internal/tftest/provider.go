34b4c2f543493cca2c71afad4b5bb69c90c8e0c4
Sean Marciniak <smarciniak@splunk.com>
2024-10-04 17:56:53 +0930

    Adding tftest package

---
+++ internal/tftest/provider.go
@@ -0,0 +1,45 @@
+package tftest
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/signalfx/signalfx-go"
+
+	pmeta "github.com/splunk-terraform/terraform-provider-signalfx/internal/providermeta"
+)
+
+// NewTestHTTPMockMeta allows for a `providermeta.Meta` to be configured as if it would be set by the provider.
+// The underlying mux uses the [http.ServerMux] using `HandlerFunc` method which allows setting the expected
+// method on routes, for example:
+//
+//	routes := map[string]http.HandlerFunc{
+//		"GET /v2/<resource>": func(...){ ... } // Only accepts GET method request matching the route
+//		"/v2/login": func(...){ ... }          // Handles all request for `/v2/login` regardless of method
+//	}
+func NewTestHTTPMockMeta(routes map[string]http.HandlerFunc) func(testing.TB) any {
+	mux := http.NewServeMux()
+	for pattern, handler := range routes {
+		mux.HandleFunc(pattern, handler)
+	}
+
+	return func(t testing.TB) any {
+		s := httptest.NewServer(mux)
+		// Ensure that the server is correctly shutdown
+		// once the test is done to avoid leaking resources
+		t.Cleanup(s.Close)
+
+		sfx, _ := signalfx.NewClient(
+			t.Name(),
+			signalfx.HTTPClient(s.Client()),
+			signalfx.APIUrl(s.URL),
+		)
+
+		return &pmeta.Meta{
+			APIURL:       s.URL,
+			CustomAppURL: s.URL,
+			Client:       sfx,
+		}
+	}
+}
---
+++ internal/tftest/provider_test.go
@@ -0,0 +1,33 @@
+package tftest
+
+import (
+	"context"
+	"io"
+	"net/http"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	pmeta "github.com/splunk-terraform/terraform-provider-signalfx/internal/providermeta"
+)
+
+func TestNewHTTPMockStat(t *testing.T) {
+	t.Parallel()
+
+	fn := NewTestHTTPMockMeta(map[string]http.HandlerFunc{
+		"/v2/team/001": func(w http.ResponseWriter, r *http.Request) {
+			_, _ = io.Copy(io.Discard, r.Body)
+			_ = r.Body.Close()
+
+			w.WriteHeader(http.StatusNoContent)
+		},
+	})
+
+	meta := fn(t)
+
+	client, err := pmeta.LoadClient(context.Background(), meta)
+	require.NoError(t, err, "Must not error loading client")
+
+	require.NoError(t, client.DeleteTeam(context.Background(), "001"), "Must not error removing team")
+	require.Error(t, client.DeleteTeam(context.Background(), "002"), "Must error trying to make request to endpoint not defined in mock")
+}
