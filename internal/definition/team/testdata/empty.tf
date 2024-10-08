f6a6d4ff9b2ce21361158ac47dd8f10433e72dca
Sean Marciniak <smarciniak@splunk.com>
2024-10-08 17:17:02 +1030

    Fixing mod to fix http server mux settings

--- internal/definition/team/testdata/empty.tf
+++
@@ -1,1 +0,0 @@
-# This file is intentionally left blank as part of the unit tests
--- internal/definition/team/testdata/missing_name.tf
+++
@@ -1,8 +0,0 @@
-provider "signalfx" {
-  // No values are provided within tests, 
-  // but refer to docs for required values to be set
-}
-
-resource "signalfx_team" "my-test-team" {
-  provider = "signalfx" // typically not required but used within testing 
-}
