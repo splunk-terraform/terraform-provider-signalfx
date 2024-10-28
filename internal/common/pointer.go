88be04f0890816e82700c70182e65d097eb37869
Sean Marciniak <smarciniak@splunk.com>
2024-10-28 13:42:58 +1030

    Adding pointer on condition

--- internal/common/pointer.go
+++ internal/common/pointer.go
@@ -6,3 +6,10 @@ package common
 func AsPointer[T any](t T) *T {
 	return &t
 }
+
+func AsPointerOnCondition[T any](t T, cond func(val T) bool) *T {
+	if !cond(t) {
+		return nil
+	}
+	return AsPointer[T](t)
+}
--- internal/common/pointer_test.go
+++ internal/common/pointer_test.go
@@ -16,3 +16,35 @@ func TestAsPointer(t *testing.T) {
 	assert.IsType(t, (*int)(nil), AsPointer(val), "Must match the expected type")
 	assert.Equal(t, &val, AsPointer(val), "Must match the expected value")
 }
+
+func TestAsPointerOnCondition(t *testing.T) {
+	t.Parallel()
+
+	for _, tc := range []struct {
+		name   string
+		val    int
+		cond   func(int) bool
+		expect *int
+	}{
+		{
+			name: "condition true",
+			val:  10,
+			cond: func(i int) bool {
+				return i > 0
+			},
+			expect: AsPointer(10),
+		},
+		{
+			name:   "condition false",
+			val:    7,
+			cond:   func(i int) bool { return i%2 == 0 },
+			expect: nil,
+		},
+	} {
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+
+			assert.Equal(t, tc.expect, AsPointerOnCondition(tc.val, tc.cond))
+		})
+	}
+}
