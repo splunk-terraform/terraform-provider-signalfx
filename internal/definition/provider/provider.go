bfac47b00aef985cd7fdee2c20d5b82a9d9bc184
Sean Marciniak <smarciniak@splunk.com>
2024-10-28 16:14:54 +1030

    Add new detector definition in provider

--- internal/definition/provider/provider.go
+++ internal/definition/provider/provider.go
@@ -17,6 +17,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/signalfx/signalfx-go"
 
+	"github.com/splunk-terraform/terraform-provider-signalfx/internal/definition/detector"
 	"github.com/splunk-terraform/terraform-provider-signalfx/internal/definition/dimension"
 	"github.com/splunk-terraform/terraform-provider-signalfx/internal/definition/team"
 	pmeta "github.com/splunk-terraform/terraform-provider-signalfx/internal/providermeta"
@@ -71,7 +72,8 @@ func New() *schema.Provider {
 			},
 		},
 		ResourcesMap: map[string]*schema.Resource{
-			team.ResourceName: team.NewResource(),
+			team.ResourceName:     team.NewResource(),
+			detector.ResourceName: detector.NewResource(),
 		},
 		DataSourcesMap: map[string]*schema.Resource{
 			dimension.DataSourceName: dimension.NewDataSource(),
