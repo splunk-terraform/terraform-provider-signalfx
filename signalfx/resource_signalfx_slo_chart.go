338a555eb6ce6882d53b7e9197156363df39fa43
Łukasz Picur <52419295+lukasz-picur-signalfx@users.noreply.github.com>
2024-11-25 01:54:14 +0100

    SLO chart type support (#549)
    * SLO chart type support
    
    * Fixing lint errors
    
    * Fixing formatting errors
    
    * Fixing formatting errors
    
    * Applying code review suggestions
    
    * Fixing formatting errors

---
+++ signalfx/resource_signalfx_slo_chart.go
@@ -0,0 +1,111 @@
+// Copyright Splunk, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
+package signalfx
+
+import (
+	"context"
+
+	"github.com/hashicorp/terraform-plugin-log/tflog"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	chart "github.com/signalfx/signalfx-go/chart"
+
+	pmeta "github.com/splunk-terraform/terraform-provider-signalfx/internal/providermeta"
+	tfext "github.com/splunk-terraform/terraform-provider-signalfx/internal/tfextension"
+)
+
+func sloChartResource() *schema.Resource {
+	return &schema.Resource{
+		Schema: map[string]*schema.Schema{
+			"slo_id": {
+				Type:        schema.TypeString,
+				Required:    true,
+				Description: "ID of the attached SLO",
+			},
+			"url": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "URL of the chart",
+			},
+		},
+
+		CreateContext: slochartCreate,
+		ReadContext:   slochartRead,
+		UpdateContext: slochartUpdate,
+		DeleteContext: slochartDelete,
+		Importer: &schema.ResourceImporter{
+			StateContext: schema.ImportStatePassthroughContext,
+		},
+	}
+}
+
+/*
+Use Resource object to construct json payload in order to create an SLO chart
+*/
+func getPayloadSloChart(d *schema.ResourceData) *chart.CreateUpdateSloChartRequest {
+	return &chart.CreateUpdateSloChartRequest{
+		SloId: d.Get("slo_id").(string),
+	}
+}
+
+func slochartCreate(ctx context.Context, d *schema.ResourceData, meta any) diag.Diagnostics {
+	config := meta.(*signalfxConfig)
+	payload := getPayloadSloChart(d)
+
+	tflog.Debug(ctx, "Creating SLO chart", tfext.NewLogFields().JSON("payload", payload))
+
+	sloChart, err := config.Client.CreateSloChart(ctx, payload)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+	// Since things worked, set the URL and move on
+	appURL := pmeta.LoadApplicationURL(ctx, meta, CHART_APP_PATH, sloChart.Id)
+
+	if err := d.Set("url", appURL); err != nil {
+		return diag.FromErr(err)
+	}
+	d.SetId(sloChart.Id)
+	return slochartAPIToTF(d, sloChart)
+}
+
+func slochartAPIToTF(d *schema.ResourceData, c *chart.Chart) diag.Diagnostics {
+	return diag.FromErr(d.Set("slo_id", c.SloId))
+}
+
+func slochartRead(ctx context.Context, d *schema.ResourceData, meta any) diag.Diagnostics {
+	config := meta.(*signalfxConfig)
+	sloChart, err := config.Client.GetChart(ctx, d.Id())
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	appURL := pmeta.LoadApplicationURL(ctx, meta, CHART_APP_PATH, sloChart.Id)
+
+	if err := d.Set("url", appURL); err != nil {
+		return diag.FromErr(err)
+	}
+
+	return slochartAPIToTF(d, sloChart)
+}
+
+func slochartUpdate(ctx context.Context, d *schema.ResourceData, meta any) diag.Diagnostics {
+	config := meta.(*signalfxConfig)
+	payload := getPayloadSloChart(d)
+	tflog.Debug(ctx, "Updating SLO chart", tfext.NewLogFields().JSON("payload", payload))
+
+	c, err := config.Client.UpdateSloChart(ctx, d.Id(), payload)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+	tflog.Debug(ctx, "SLO chart update response", tfext.NewLogFields().JSON("response", c))
+
+	d.SetId(c.Id)
+	return slochartAPIToTF(d, c)
+}
+
+func slochartDelete(ctx context.Context, d *schema.ResourceData, meta any) diag.Diagnostics {
+	config := meta.(*signalfxConfig)
+
+	return diag.FromErr(config.Client.DeleteChart(ctx, d.Id()))
+}
