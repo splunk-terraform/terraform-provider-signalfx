6179b65676263faa50eed5c4dccd05e2c206f050
Sean Marciniak <30928402+MovieStoreGuy@users.noreply.github.com>
2025-03-24 13:11:45 +1030

    Feature: Adding in support for encoding messages for AWS (#579)
    * Adding in support for encoding messages for AWS
    
    * Simplify errors
    
    * Adding check for Key

--- internal/definition/awsintegration/schema_integration_test.go
+++ internal/definition/awsintegration/schema_integration_test.go
@@ -176,3 +176,105 @@ func TestIntegrationDecode(t *testing.T) {
 		})
 	}
 }
+
+func TestEncodeTerraformIntegration(t *testing.T) {
+	t.Parallel()
+
+	for _, tc := range []struct {
+		name   string
+		input  *integration.AwsCloudWatchIntegration
+		errVal string
+	}{
+		{
+			name:   "empty integration",
+			input:  &integration.AwsCloudWatchIntegration{},
+			errVal: "",
+		},
+		{
+			name: "token set",
+			input: &integration.AwsCloudWatchIntegration{
+				Token: "my-token",
+			},
+			errVal: "",
+		},
+		{
+			name: "regions set",
+			input: &integration.AwsCloudWatchIntegration{
+				Regions: []string{
+					"us-east-1",
+					"us-east-2",
+				},
+			},
+			errVal: "",
+		},
+		{
+			name: "custom namespace sync rules",
+			input: &integration.AwsCloudWatchIntegration{
+				CustomNamespaceSyncRules: []*integration.AwsCustomNameSpaceSyncRule{
+					{
+						DefaultAction: integration.INCLUDE,
+						Namespace:     "AWS/Kinesis",
+						Filter: &integration.AwsSyncRuleFilter{
+							Source: "hope",
+							Action: integration.INCLUDE,
+						},
+					},
+				},
+			},
+		},
+		{
+			name: "custom namespaces",
+			input: &integration.AwsCloudWatchIntegration{
+				CustomCloudWatchNamespaces: "namespace-1,namespace-2,namespace-3",
+			},
+			errVal: "",
+		},
+		{
+			name: "services",
+			input: &integration.AwsCloudWatchIntegration{
+				Services: []integration.AwsService{
+					"my-service-01",
+					"my-service-02",
+				},
+			},
+			errVal: "",
+		},
+		{
+			name: "namespace rules",
+			input: &integration.AwsCloudWatchIntegration{
+				NamespaceSyncRules: []*integration.AwsNameSpaceSyncRule{
+					{
+						DefaultAction: integration.INCLUDE,
+						Namespace:     "AWS/Kinesis",
+						Filter: &integration.AwsSyncRuleFilter{
+							Action: integration.INCLUDE,
+							Source: "hope",
+						},
+					},
+				},
+			},
+			errVal: "",
+		},
+		{
+			name: "metric stats",
+			input: &integration.AwsCloudWatchIntegration{
+				MetricStatsToSync: map[string]map[string][]string{
+					"AWS/SQS": {
+						"ApproximateAgeOfOldestMessage": {"mean"},
+					},
+				},
+			},
+		},
+	} {
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+
+			err := encodeTerraform(tc.input, schema.TestResourceDataRaw(t, newIntegrationSchema(), map[string]any{}))
+			if tc.errVal != "" {
+				assert.EqualError(t, err, tc.errVal, "Must match the expected value")
+			} else {
+				assert.NoError(t, err, "Must not return an error")
+			}
+		})
+	}
+}
