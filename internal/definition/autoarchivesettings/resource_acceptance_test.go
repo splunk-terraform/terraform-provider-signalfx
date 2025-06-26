eb21f5cb246c21645d0c80c582d9517aa013000f
Anuj Doiphode <adoiphode@splunk.com>
2025-06-26 12:36:23 -0700

    renamed settings test data file

--- internal/definition/autoarchivesettings/resource_acceptance_test.go
+++ internal/definition/autoarchivesettings/resource_acceptance_test.go
@@ -18,17 +18,11 @@ func TestResourceAcceptance(t *testing.T) {
 			name: "basic automated archival settings",
 			steps: []resource.TestStep{
 				{
-					Config: tftest.LoadConfig("testdata/setting.tf"),
+					Config: tftest.LoadConfig("testdata/resource_setting.tf"),
 					Check: resource.ComposeAggregateTestCheckFunc(
-						resource.TestCheckResourceAttr("signalfx_auto_archive_settings.setting", "creator", "Test Creator"),
-						resource.TestCheckResourceAttr("signalfx_auto_archive_settings.setting", "last_updated_by", "Test Creator"),
-						resource.TestCheckResourceAttr("signalfx_auto_archive_settings.setting", "created", "12340"),
-						resource.TestCheckResourceAttr("signalfx_auto_archive_settings.setting", "last_updated", "12345"),
-						resource.TestCheckResourceAttr("signalfx_auto_archive_settings.setting", "version", "1"),
 						resource.TestCheckResourceAttr("signalfx_auto_archive_settings.setting", "enabled", "true"),
 						resource.TestCheckResourceAttr("signalfx_auto_archive_settings.setting", "lookback_period", "P30D"),
 						resource.TestCheckResourceAttr("signalfx_auto_archive_settings.setting", "grace_period", "P15D"),
-						resource.TestCheckResourceAttr("signalfx_auto_archive_settings.setting", "ruleset_limit", "1000"),
 					),
 					ExpectNonEmptyPlan: false,
 				},
