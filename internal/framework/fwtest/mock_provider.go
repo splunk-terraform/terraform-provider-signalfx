3d01e60b0c7e03932250dea90ed99975c26ccbf3
Sean Marciniak <30928402+MovieStoreGuy@users.noreply.github.com>
2025-08-11 15:26:56 +0930

    Chore: Adding package for framework validation (#639)
    * Adding package for framework validation
    
    This package is inteded to simplify the work being done within the
    components
    
    * Adding comment regarding changes
    
    * Fixing the typo

---
+++ internal/framework/fwtest/mock_provider.go
@@ -0,0 +1,113 @@
+// Copyright Splunk, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
+package fwtest
+
+import (
+	"context"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-framework/datasource"
+	"github.com/hashicorp/terraform-plugin-framework/provider"
+	"github.com/hashicorp/terraform-plugin-framework/provider/schema"
+	"github.com/hashicorp/terraform-plugin-framework/providerserver"
+	"github.com/hashicorp/terraform-plugin-framework/resource"
+	"github.com/hashicorp/terraform-plugin-go/tfprotov5"
+	"github.com/signalfx/signalfx-go"
+
+	pmeta "github.com/splunk-terraform/terraform-provider-signalfx/internal/providermeta"
+)
+
+type MockProvider struct {
+	data *pmeta.Meta
+
+	resources   []func() resource.Resource
+	datasources []func() datasource.DataSource
+}
+
+var (
+	_ provider.Provider = (*MockProvider)(nil)
+)
+
+func WithMockResources(resources ...func() resource.Resource) func(*MockProvider) {
+	return func(mp *MockProvider) {
+		mp.resources = resources
+	}
+}
+
+func WithMockDataSources(datasources ...func() datasource.DataSource) func(*MockProvider) {
+	return func(mp *MockProvider) {
+		mp.datasources = datasources
+	}
+}
+
+func NewMockProviderFactory(tb testing.TB, endpoints map[string]http.Handler, opts ...func(*MockProvider)) map[string]func() (tfprotov5.ProviderServer, error) {
+	mux := http.NewServeMux()
+	for path, handler := range endpoints {
+		mux.Handle(path, handler)
+	}
+	// The pattern matchers will match based on the longest prefix matching
+	// so this acts to help identify unmatched paths and will force the test
+	// to fail so it the behavior is not dependant on.
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		tb.Log("Unhandled request:", r.Method, r.URL.Path)
+
+		http.Error(w, "Internal Test Error: "+tb.Name(), http.StatusInternalServerError)
+
+		tb.Fail()
+	})
+
+	s := httptest.NewServer(mux)
+	tb.Cleanup(s.Close)
+
+	client, _ := signalfx.NewClient(
+		tb.Name(),
+		signalfx.HTTPClient(s.Client()),
+		signalfx.APIUrl(s.URL),
+	)
+
+	mock := &MockProvider{
+		data: &pmeta.Meta{
+			Client:       client,
+			APIURL:       s.URL,
+			CustomAppURL: s.URL,
+		},
+	}
+
+	for _, opt := range opts {
+		opt(mock)
+	}
+
+	return map[string]func() (tfprotov5.ProviderServer, error){
+		"signalfx": func() (tfprotov5.ProviderServer, error) { //nolint:unparam // required signature for provider server
+			return providerserver.NewProtocol5(mock)(), nil
+		},
+	}
+}
+
+func (mp MockProvider) Metadata(ctx context.Context, req provider.MetadataRequest, resp *provider.MetadataResponse) {
+	resp.TypeName = "signalfx"
+	resp.Version = "1.0.0"
+}
+
+func (mp MockProvider) Schema(_ context.Context, _ provider.SchemaRequest, resp *provider.SchemaResponse) {
+	resp.Schema = schema.Schema{
+		Description: "This is a mock provider for testing purposes.",
+	}
+}
+
+func (mp MockProvider) Configure(_ context.Context, _ provider.ConfigureRequest, resp *provider.ConfigureResponse) {
+	resp.ResourceData = mp.data
+	resp.DataSourceData = mp.data
+	resp.EphemeralResourceData = mp.data
+}
+
+func (mp MockProvider) DataSources(ctx context.Context) []func() datasource.DataSource {
+	return mp.datasources
+}
+
+func (mp MockProvider) Resources(ctx context.Context) []func() resource.Resource {
+	return mp.resources
+}
---
+++ internal/framework/fwtest/schema_validation_test.go
@@ -0,0 +1,171 @@
+// Copyright Splunk, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
+package fwtest
+
+import (
+	"context"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-framework/resource"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+	"github.com/stretchr/testify/assert"
+)
+
+type ResourceMock struct {
+	resource.Resource
+
+	schema schema.Schema
+}
+
+func (rm ResourceMock) Schema(_ context.Context, _ resource.SchemaRequest, resp *resource.SchemaResponse) {
+	resp.Schema = rm.schema
+}
+
+func TestResourceSchemaValidate(t *testing.T) {
+	t.Parallel()
+
+	for _, tc := range []struct {
+		name   string
+		res    ResourceMock
+		model  any
+		expect string
+	}{
+		{
+			name: "Invalid model type provided",
+			res: ResourceMock{
+				schema: schema.Schema{
+					Description: "Test schema",
+					Attributes: map[string]schema.Attribute{
+						"id": schema.StringAttribute{
+							Required: true,
+						},
+					},
+				},
+			},
+			model:  "not a struct",
+			expect: "model must be a struct, provided: string",
+		},
+		{
+			name: "Missing schema definition within model",
+			res: ResourceMock{
+				schema: schema.Schema{
+					Description: "Test schema",
+					Attributes: map[string]schema.Attribute{
+						"id": schema.StringAttribute{
+							Required: true,
+						},
+					},
+				},
+			},
+			model:  struct{}{},
+			expect: `expected field not found in model: "id", check struct tags`,
+		},
+		{
+			name: "Valid schema and model",
+			res: ResourceMock{
+				schema: schema.Schema{
+					Description: "Test schema",
+					Attributes: map[string]schema.Attribute{
+						"id": schema.StringAttribute{
+							Required: true,
+						},
+					},
+				},
+			},
+			model: struct {
+				Id types.String `tfsdk:"id"`
+			}{},
+		},
+		{
+			name: "Additional values defined in model not present in schema",
+			res: ResourceMock{
+				schema: schema.Schema{
+					Description: "Test schema",
+					Attributes: map[string]schema.Attribute{
+						"id": schema.StringAttribute{
+							Required: true,
+						},
+					},
+				},
+			},
+			model: struct {
+				Id       types.String `tfsdk:"id"`
+				fastmode types.Bool   `tfsdk:"fastmode"`
+			}{},
+			expect: `framework requires field exported "fastmode"`,
+		},
+		{
+			name: "Missed defining description in schema with missing struct tag",
+			res: ResourceMock{
+				schema: schema.Schema{
+					Attributes: map[string]schema.Attribute{
+						"id": schema.StringAttribute{
+							Required: true,
+						},
+					},
+				},
+			},
+			model:  struct{ id types.String }{},
+			expect: `missing schema description; expected field not found in model: "id", check struct tags`,
+		},
+		{
+			name: "Missed defining attributes in schema",
+			res: ResourceMock{
+				schema: schema.Schema{
+					Description: "Test schema",
+					Attributes:  nil,
+				},
+			},
+			model:  &struct{ id types.String }{},
+			expect: "missing schema attribute definitions",
+		},
+		{
+			name: "Model define additional fields not defined in schema",
+			res: ResourceMock{
+				schema: schema.Schema{
+					Description: "Test schema",
+					Attributes: map[string]schema.Attribute{
+						"id": schema.StringAttribute{
+							Required: true,
+						},
+					},
+				},
+			},
+			model: struct {
+				Id    types.String `tfsdk:"id"`
+				Extra types.String `tfsdk:"extra"`
+			}{},
+			expect: `additional field defined in model but not defined: "extra"`,
+		},
+		{
+			name: "Mismatched field type in schema and model",
+			res: ResourceMock{
+				schema: schema.Schema{
+					Description: "Test schema",
+					Attributes: map[string]schema.Attribute{
+						"id": schema.StringAttribute{
+							Required: true,
+						},
+					},
+				},
+			},
+			model: &struct {
+				Id types.Int64 `tfsdk:"id"`
+			}{},
+			expect: `field "id" has type "basetypes.StringType", expected "basetypes.Int64Type"`,
+		},
+	} {
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+
+			err := ResourceSchemaValidate(tc.res, tc.model)
+			if tc.expect == "" {
+				assert.NoError(t, err, "Must not return an error")
+			} else {
+				assert.EqualError(t, err, tc.expect, "Must return the expected error")
+			}
+		})
+	}
+}
