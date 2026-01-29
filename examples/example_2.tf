2ebba7b53edbea64fead4d8419d58f1f8436428f
Sean Marciniak <smarciniak@splunk.com>
2026-01-29 16:10:07 +1030

    Updating docs to reference the latest domain

--- examples/example_2.tf
+++ examples/example_2.tf
@@ -4,7 +4,7 @@ provider "signalfx" {
   password        = "${var.service_account_password}"
   organization_id = "${var.service_account_org_id}"
   # If your organization uses a different realm
-  # api_url = "https://api.<realm>.signalfx.com"
+  # api_url = "https://api.<realm>.observability.splunk.com"
   # If your organization uses a custom URL
-  # custom_app_url = "https://myorg.signalfx.com"
+  # custom_app_url = "https://myorg.observability.splunk.com"
 }
